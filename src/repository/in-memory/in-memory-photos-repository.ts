5e29047ca10a435ffe338069c20d2c4187edb2eb
cleyber <cleyber.fernandes@gmail.com>
2025-11-13 10:26:32 -0300

    chore: implement in-memory repositories for register photos and pets

---
+++ src/repository/in-memory/in-memory-photos-repository.ts
@@ -0,0 +1,16 @@
+import type { PhotosRepositoryInterface } from '../photos-repository-interface'
+import { type Photo, Prisma } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryPhotosRepository implements PhotosRepositoryInterface {
+  private items: Photo[] = []
+  async create(data: Prisma.PhotoUncheckedCreateInput) {
+    const photo = {
+      id: randomUUID(),
+      ...data,
+    }
+
+    this.items.push(photo)
+    return photo
+  }
+}
